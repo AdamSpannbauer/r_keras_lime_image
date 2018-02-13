0b0902b7a0d36c34f0b5a4502c31f92660800258
Adam Spannbauer <SpannbauerAdam@gmail.com>
2018-02-13 09:34:02 -0500

    converted output to keep original class prediction

--- keras_vgg19_cat_dog.R
+++ keras_vgg19_cat_dog.R
@@ -45,17 +45,16 @@ preds = lapply(image_list, function(i) {
 #------------------------------------------------
 
 #####################
-# convert labels to 'Cat', 'Dog', 'NoCatDog'
+# add dog/cat labels
 #####################
 dog_labs = read_dog_cat_labels('data/dog_classes.txt')
 cat_labs = read_dog_cat_labels('data/cat_classes.txt')
 
-#convert all dog / cat breeds to just 'dog' and 'cat'
+#add label for 'dog' and 'cat'
 preds = lapply(preds, function(df_i) {
-  df_i$class_description[df_i$class_description %in% dog_labs] = 'Dog'
-  df_i$class_description[df_i$class_description %in% cat_labs] = 'Cat'
-  df_i$class_description[df_i$class_description != 'Dog' &
-                           df_i$class_description != 'Cat'] = 'NoCatDog'
+  df_i$catdog = NA
+  df_i$catdog[df_i$class_description %in% dog_labs] = 'Dog'
+  df_i$catdog[df_i$class_description %in% cat_labs] = 'Cat'
   df_i
 })
 #------------------------------------------------
@@ -67,12 +66,12 @@ pred_df = do.call(rbind, preds)
 pred_df$class_name = NULL
 pred_df$file_name = unlist(image_paths)
 
-#   class_description     score                                  file_name
-# 1               Cat 0.4494216            images/cats/goober_lounging.JPG
-# 2               Cat 0.4777499           images/cats/google_tabby_cat.jpg
-# 3               Dog 0.3038349       images/cats/jasper_announce_sign.jpg
-# 4               Cat 0.1475925                images/cats/lilly_perch.JPG
-# 5               Dog 0.5802087        images/dogs/tonks_announce_sign.jpg
-# 6               Dog 0.7851309 images/dogs/tonks_jasper_announce_sign.jpg
-# 7               Dog 0.9420919        images/dogs/tonks_proposal_sign.jpg
+#   class_description     score catdog                                  file_name
+# 1      Egyptian_cat 0.4494216    Cat            images/cats/goober_lounging.JPG
+# 2             tabby 0.4777499    Cat           images/cats/google_tabby_cat.jpg
+# 3    Scotch_terrier 0.3038349    Dog       images/cats/jasper_announce_sign.jpg
+# 4              lynx 0.1475925    Cat                images/cats/lilly_perch.JPG
+# 5          malinois 0.5802087    Dog        images/dogs/tonks_announce_sign.jpg
+# 6         Chihuahua 0.7851309    Dog images/dogs/tonks_jasper_announce_sign.jpg
+# 7         Chihuahua 0.9420919    Dog        images/dogs/tonks_proposal_sign.jpg
 #------------------------------------------------
