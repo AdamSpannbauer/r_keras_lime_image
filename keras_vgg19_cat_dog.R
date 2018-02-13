eb3d73d2155ec7ec86a07a1a8aa8b26346df5374
Adam Spannbauer <SpannbauerAdam@gmail.com>
2018-02-13 16:20:38 -0500

    created python script copying r script cat dog classification

--- keras_vgg19_cat_dog.R
+++ keras_vgg19_cat_dog.R
@@ -23,10 +23,8 @@ read_dog_cat_labels = function(path) {
 #####################
 # prep images for classification
 #####################
-# define image path to classify
-image_paths = list.files('images', 
-                         recursive = TRUE, 
-                         full.names = TRUE)
+# define image paths to classify
+image_paths = list.files('images', recursive = TRUE, full.names = TRUE)
 
 # preprocess images
 image_list = lapply(image_paths, image_preprocessor)
@@ -42,36 +40,40 @@ model = application_vgg19()
 preds = lapply(image_list, function(i) {
   imagenet_decode_predictions(predict(model, i), top = 1)[[1]]
 })
+
+# convert list of predictions to df and drop class name column
+pred_df = do.call(rbind, preds)
+pred_df$class_name = NULL
 #------------------------------------------------
 
 #####################
-# add dog/cat labels
+# add dog/cat labels, add file name, sort by score
 #####################
+#read in breed labels
 dog_labs = read_dog_cat_labels('data/dog_classes.txt')
 cat_labs = read_dog_cat_labels('data/cat_classes.txt')
 
-#add label for 'dog' and 'cat'
-preds = lapply(preds, function(df_i) {
-  df_i$catdog = NA
-  df_i$catdog[df_i$class_description %in% dog_labs] = 'Dog'
-  df_i$catdog[df_i$class_description %in% cat_labs] = 'Cat'
-  df_i
-})
+#create column for labeling dog breeds as dog and cat breeds as cat
+pred_df$catdog = NA
+pred_df$catdog[pred_df$class_description %in% dog_labs] = 'Dog'
+pred_df$catdog[pred_df$class_description %in% cat_labs] = 'Cat'
+
+#add column with image paths
+pred_df$file_name = unlist(image_paths)
+pred_df = pred_df[order(-pred_df$score),]
 #------------------------------------------------
 
 #####################
-# pretty up output
+# print output
 #####################
-pred_df = do.call(rbind, preds)
-pred_df$class_name = NULL
-pred_df$file_name = unlist(image_paths)
+print(pred_df)
 
 #   class_description     score catdog                                  file_name
-# 1      Egyptian_cat 0.4494216    Cat            images/cats/goober_lounging.JPG
-# 2             tabby 0.4777499    Cat           images/cats/google_tabby_cat.jpg
-# 3    Scotch_terrier 0.3038349    Dog       images/cats/jasper_announce_sign.jpg
-# 4              lynx 0.1475925    Cat                images/cats/lilly_perch.JPG
-# 5          malinois 0.5802087    Dog        images/dogs/tonks_announce_sign.jpg
-# 6         Chihuahua 0.7851309    Dog images/dogs/tonks_jasper_announce_sign.jpg
 # 7         Chihuahua 0.9420919    Dog        images/dogs/tonks_proposal_sign.jpg
+# 6         Chihuahua 0.7851309    Dog images/dogs/tonks_jasper_announce_sign.jpg
+# 5          malinois 0.5802087    Dog        images/dogs/tonks_announce_sign.jpg
+# 2             tabby 0.4777499    Cat           images/cats/google_tabby_cat.jpg
+# 1      Egyptian_cat 0.4494216    Cat            images/cats/goober_lounging.jpg
+# 3    Scotch_terrier 0.3038349    Dog       images/cats/jasper_announce_sign.jpg
+# 4              lynx 0.1475925    Cat                images/cats/lilly_perch.jpg
 #------------------------------------------------
